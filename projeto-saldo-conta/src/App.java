e9dae92ddab987e4ce27d264feb953dd0edbdf04
Ghust <andregustavoteixeira58@gmail.com>
2025-03-01 15:35:00 -0300

    .

--- projeto-saldo-conta/src/App.java
+++ projeto-saldo-conta/src/App.java
@@ -1,5 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("qual o saldo inicial da conta?");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Qual o valor da primeira transacao? ");
+        double primeiraTransacao = scanner.nextDouble();
+        System.out.println("Qual o valor da segunda transacao? ");
+        double segundaTransacao = scanner.nextDouble();
+        System.out.println("Qual o valor da terceira transacao? ");
+        double terceiraTransacao = scanner.nextDouble();
+
+        double novoSaldo = calcularNovoSaldo(saldo,primeiraTransacao,segundaTransacao,terceiraTransacao);
+        System.out.println(novoSaldo);
+    }
+    public static double calcularNovoSaldo(double saldo, double primeiraTransacao, double segundaTransacao, double terceiraTransacao){
+        double novoSaldo = saldo + primeiraTransacao + segundaTransacao + terceiraTransacao;
+        return novoSaldo;
     }
 }
